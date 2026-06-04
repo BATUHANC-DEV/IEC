6707248f6f9e42b4eeb94ddf213f9258da6bd838
BATUHANC-DEV <230316063@ogr.cbu.edu.tr>
2026-06-04 18:07:15 +0300

    Update Otomasyon.tsx

--- src/app/pages/Otomasyon.tsx
+++ src/app/pages/Otomasyon.tsx
@@ -1,7 +1,7 @@
 import { motion } from "motion/react";
 import { CheckCircle2, ArrowLeft } from "lucide-react";
 import { Link } from "react-router";
-import otomasyonImage from "../../assets/924dad9e368e36bc14b0fda5365ab4ce9aad1fe1.png";
+import otoImage from "../../assets/oto1.jpg";
 
 export function Otomasyon() {
   const services = [
