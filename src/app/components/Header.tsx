f50ae8a91e5f86cc8407a532b93babadd4d89981
BATUHANC-DEV <230316063@ogr.cbu.edu.tr>
2026-06-04 13:32:18 +0300

    Update Header.tsx

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
 import { Menu, X, Phone, Mail } from "lucide-react";
 import { Button } from "@/app/components/ui/button";
-import logo from "../../../assets/logo.png";
+import logo from "../../../assets/yeni-logo.png";
 
 export function Header() {
   const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
